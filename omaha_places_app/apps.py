02de567aa653b5c18038800882619f838da90b7b
Ysa <ihennysan@gmail.com>
2025-03-20 11:41:26 -0500

    changed app name to omaha_places_app

--- omaha_places_app/apps.py
+++ omaha_places_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HelloWorldAppConfig(AppConfig):
+class OmahaPlacesAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hello_world_app'
+    name = 'omaha_places_app'
