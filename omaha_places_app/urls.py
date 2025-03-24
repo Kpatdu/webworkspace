2d8c239c9f72db1f43ce3409a253970a5e546677
Ysa <ihennysan@gmail.com>
2025-03-24 16:25:08 -0500

    Renamed hello_world_app to omaha_places_app, added new basic views for home, about us, restaurants, etc., added respective template models, updated models.py

---
+++ omaha_places_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name = 'home'), # Home
+    path('restaurants/', views.RestaurantsView.as_view(), name = 'restaurants'), # Restaurants
+    path('places', views.PlacesView.as_view(), name = 'places'), # Places
+    path('about/', views.AboutUsView.as_view(), name = 'about'), # About Us
+    path('contact/', views.ContactUsView.as_view(), name = 'contact'), # Cotnact Us
+]
