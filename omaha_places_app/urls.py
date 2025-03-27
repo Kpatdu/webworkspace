1acba8dd42f5aff36d7ec8ce923ef68140175a97
Ysa <ihennysan@gmail.com>
2025-03-26 19:58:52 -0500

    Pulled changes from main

--- omaha_places_app/urls.py
+++ omaha_places_app/urls.py
@@ -5,9 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name = 'home'), # Home
-    path('restaurants/all/', views.RestaurantsView.as_view(), name = 'all_restaurants'), # List Restaurants
-    path('restaurants/<int:pk>/', views.RestaurantDetailView.as_view(), name = 'restaurant_detail'), # Individual Restaurant
-    path('places/all/', views.PlacesView.as_view(), name = 'all_places'), # List Places
-    path('places/<int:pk>/', views.PlaceDetailView.as_view(), name = 'place_detail'), # IndividualPlace
     path('about/', views.AboutUsView.as_view(), name = 'about'), # About Us
+
+    path('restaurants/home/', views.RestaurantsView.as_view(), name = 'home_restaurants'), # List Restaurants
+    path('restaurants/<int:pk>/', views.RestaurantDetailView.as_view(), name = 'restaurant_detail'), # Individual Restaurant
+    path('places/home/', views.PlacesView.as_view(), name = 'home_places'), # List Places
+    path('places/<int:pk>/', views.PlaceDetailView.as_view(), name = 'place_detail'), # IndividualPlace
+
+    path('locations/', views.LocationsView.as_view(), name = 'locations') # Locations
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
