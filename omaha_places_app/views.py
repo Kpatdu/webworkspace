22292d9e091dff9c7c6c5fa8980186234d7511db
Ysa <ihennysan@gmail.com>
2025-03-27 20:05:56 -0500

    Removed random function from views and now displaying a random image is handled in home.html. Cleaned up code in views.py. Removed the cache folder from .gitignore. Made navbar pure black.

--- omaha_places_app/views.py
+++ omaha_places_app/views.py
@@ -3,23 +3,54 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
 from .models import *
 
-import random
+import os
+from dotenv import load_dotenv
+from urllib.parse import unquote
 
 
 class HomeView(TemplateView):
+    '''
+    Class-based view to render the home page.
+    '''
+
     template_name = 'omaha_places_app/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
+
         # Get all restaurant and place images
         restaurant_images = list(Restaurant.objects.exclude(image__isnull = True).exclude(image = 'N/A').values_list('image', flat = True))
         place_images = list(Place.objects.exclude(image__isnull = True).exclude(image = 'N/A').values_list('image', flat = True))
 
         all_images = restaurant_images + place_images
-        context['all_images'] = all_images
+
+        dotenv_path = r'omaha_places_app\cache\googleapi.env'
+        load_dotenv(dotenv_path=dotenv_path)
+        GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+        # Ensure the API key is correctly replaced
+        if GOOGLE_API_KEY:
+            all_images_with_api_key = [
+                self.replace_api_key(image, GOOGLE_API_KEY)
+                for image in all_images
+            ]
+        else:
+            all_images_with_api_key = all_images  # If no API key, use the image URLs as is
+
+        context['all_images'] = all_images_with_api_key
 
         return context
+
+    def replace_api_key(self, image_url, api_key):
+        '''
+        Replace the placeholder GOOGLE_API_KEY in the image URL with the actual API key.
+        '''
+
+        image_url = image_url.replace('GOOGLE_API_KEY', api_key)
+        image_url = unquote(image_url)
+
+        return image_url
+
 
 
 class AboutUsView(TemplateView):
@@ -57,7 +88,7 @@ class RestaurantDetailView(TemplateView):
     template_name = 'omaha_places_app/restaurant.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['restaurant'] = Restaurant.objects.get(id=self.kwargs['pk'])
+        context['restaurant'] = Restaurant.objects.get(id = self.kwargs['pk'])
 
         return context
 
@@ -80,7 +111,7 @@ class PlaceDetailView(TemplateView):
     template_name = 'omaha_places_app/place.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['place'] = Place.objects.get(id=self.kwargs['pk'])
+        context['place'] = Place.objects.get(id = self.kwargs['pk'])
 
         return context
 
@@ -105,4 +136,5 @@ class LocationsView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['all_restaurants'] = Restaurant.objects.all()
         context['all_places'] = Place.objects.all()
+
         return context
