57ec6170a99f90ffb394731e5a751043a3d8513a
TammyLe175 <129108593+TammyLe175@users.noreply.github.com>
2025-04-06 20:39:46 -0500

    add js folder in static NOTE:js file fails to load

---
+++ static/js/places-home.js
@@ -0,0 +1,7 @@
+const title = document.createElement("title");
+
+title.style.color = "red";
+title.style.backgroundColor= "blue";
+document.body.appendChild(title)
+
+console.log("JS file is linked correctly!");
