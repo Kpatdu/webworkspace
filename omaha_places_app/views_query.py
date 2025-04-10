4dcd5d0ae44c506b59554ddd1050a7b23723cd7f
jmartin74 <jmartin74@unomaha.edu>
2025-04-10 00:26:17 -0500

    added search capabilities to navbar and filter search forms in \all-locations\

--- omaha_places_app/views_query.py
+++ omaha_places_app/views_query.py
@@ -16,3 +16,48 @@ class LocationsView(TemplateView):
         context['all_places'] = Place.objects.all()
 
         return context
+    
+    def post(self, request, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.method == 'POST':
+            q = request.POST['q']
+            context['all_restaurants'] = Restaurant.objects.filter(name__contains=q) | Restaurant.objects.filter(category__contains=q)
+            context['all_places'] = Place.objects.filter(name__contains=q) | Place.objects.filter(category__contains=q)
+            return render(request, self.template_name, context)
+        
+    def get(self, request, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.method == 'GET':
+            name_contains_query = request.GET.get('name_contains')
+            cat_contains_query = request.GET.get('cat_contains')
+            price_min = request.GET.get('price_min')
+            price_max = request.GET.get('price_max')
+            rating_min = request.GET.get('rating_min')
+            rating_max = request.GET.get('rating_max')
+
+            context['all_restaurants'] = Restaurant.objects.all()
+            context['all_places'] = Place.objects.all()
+
+
+            if name_contains_query != '' and name_contains_query is not None:
+                context['all_restaurants'] = context['all_restaurants'].filter(name__contains=name_contains_query)
+                context['all_places'] = context['all_places'].filter(name__contains=name_contains_query)
+            if cat_contains_query != '' and cat_contains_query is not None:
+                print('hi')
+                context['all_restaurants'] = context['all_restaurants'].filter(category__contains=cat_contains_query)
+                context['all_places'] = context['all_places'].filter(category__contains=cat_contains_query)
+            if price_min != '' and price_min is not None:
+                context['all_restaurants'] = context['all_restaurants'].filter(price__gte=price_min)
+                context['all_places'] = context['all_places'].filter(price__gte=price_min)
+            if price_max != '' and price_max is not None:
+                context['all_restaurants'] = context['all_restaurants'].filter(price__lte=price_max)
+                context['all_places'] = context['all_places'].filter(price__lte=price_max)
+            if rating_min != '' and rating_min is not None:
+                context['all_restaurants'] = context['all_restaurants'].filter(rating__gte=rating_min)
+                context['all_places'] = context['all_places'].filter(rating__gte=rating_min)
+            if rating_max != '' and rating_max is not None:
+                context['all_restaurants'] = context['all_restaurants'].filter(rating__lte=rating_max)
+                context['all_places'] = context['all_places'].filter(rating__lte=rating_max)
+            
+
+            return render(request, self.template_name, context)
