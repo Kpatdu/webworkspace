685af1e75130970548548c2f36b38104ba75f7a7
Ysa <ihennysan@gmail.com>
2025-04-25 18:34:44 -0500

    Prep to replace expired API key; finished search system + UI changes

--- omaha_places_app/views_query.py
+++ omaha_places_app/views_query.py
@@ -1,63 +1,91 @@
+from .models import Restaurant, Place
+from .mixins import RestaurantImageAPIKeyMixin, PlaceImageAPIKeyMixin
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import Restaurant, Place
+
+from django.db.models import Q
+from django.http import QueryDict
 
 
-class LocationsView(TemplateView):
+class LocationsView(TemplateView, RestaurantImageAPIKeyMixin, PlaceImageAPIKeyMixin):
     '''
-    Class-based view to display all restaurants and places together.
+    Class-based view for displaying all locations (restaurants and places).
     '''
-    
+
     template_name = 'omaha_places_app/all-locations.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['all_restaurants'] = Restaurant.objects.all()
-        context['all_places'] = Place.objects.all()
+
+        # Get the restaurant and place images with the API key
+        restaurants = self.get_restaurant_images_with_api_key()[1]
+        places = self.get_place_images_with_api_key()[1]
+
+        context['all_restaurants'] = restaurants
+        context['all_places'] = places
+
+        # Parse all unique categories from restaurant and place objects
+        category_set = set()
+        for r in restaurants:
+            if r.category:
+                category_set.update([cat.strip() for cat in r.category.split(',')])
+        for p in places:
+            if p.category:
+                category_set.update([cat.strip() for cat in p.category.split(',')])
+        context['all_categories'] = sorted(list(category_set))
+
+        # Get filters from request and pass them to the context
+        context['selected_categories'] = self.request.GET.getlist('cat_contains')
 
         return context
-    
-    def post(self, request, **kwargs):
-        context = self.get_context_data(**kwargs)
-        if request.method == 'POST':
-            q = request.POST['q']
-            context['all_restaurants'] = Restaurant.objects.filter(name__contains=q) | Restaurant.objects.filter(category__contains=q)
-            context['all_places'] = Place.objects.filter(name__contains=q) | Place.objects.filter(category__contains=q)
-            return render(request, self.template_name, context)
-        
+
     def get(self, request, **kwargs):
         context = self.get_context_data(**kwargs)
-        if request.method == 'GET':
-            name_contains_query = request.GET.get('name_contains')
-            cat_contains_query = request.GET.get('cat_contains')
-            price_min = request.GET.get('price_min')
-            price_max = request.GET.get('price_max')
-            rating_min = request.GET.get('rating_min')
-            rating_max = request.GET.get('rating_max')
 
-            context['all_restaurants'] = Restaurant.objects.all()
-            context['all_places'] = Place.objects.all()
+        # Get filters from request
+        name_contains = request.GET.get('name_contains')
+        cat_contains_list = request.GET.getlist('cat_contains')
+        price_min = request.GET.get('price_min')
+        price_max = request.GET.get('price_max')
+        rating_min = request.GET.get('rating_min')
+        rating_max = request.GET.get('rating_max')
+        description_contains = request.GET.get('description_contains')
 
+        # Start with all items
+        restaurants = context['all_restaurants']
+        places = context['all_places']
 
-            if name_contains_query != '' and name_contains_query is not None:
-                context['all_restaurants'] = context['all_restaurants'].filter(name__contains=name_contains_query)
-                context['all_places'] = context['all_places'].filter(name__contains=name_contains_query)
-            if cat_contains_query != '' and cat_contains_query is not None:
-                print('hi')
-                context['all_restaurants'] = context['all_restaurants'].filter(category__contains=cat_contains_query)
-                context['all_places'] = context['all_places'].filter(category__contains=cat_contains_query)
-            if price_min != '' and price_min is not None:
-                context['all_restaurants'] = context['all_restaurants'].filter(price__gte=price_min)
-                context['all_places'] = context['all_places'].filter(price__gte=price_min)
-            if price_max != '' and price_max is not None:
-                context['all_restaurants'] = context['all_restaurants'].filter(price__lte=price_max)
-                context['all_places'] = context['all_places'].filter(price__lte=price_max)
-            if rating_min != '' and rating_min is not None:
-                context['all_restaurants'] = context['all_restaurants'].filter(rating__gte=rating_min)
-                context['all_places'] = context['all_places'].filter(rating__gte=rating_min)
-            if rating_max != '' and rating_max is not None:
-                context['all_restaurants'] = context['all_restaurants'].filter(rating__lte=rating_max)
-                context['all_places'] = context['all_places'].filter(rating__lte=rating_max)
-            
+        # Apply filters
+        if name_contains:
+            restaurants = restaurants.filter(name__icontains=name_contains)
+            places = places.filter(name__icontains=name_contains)
 
-            return render(request, self.template_name, context)
+        if cat_contains_list:
+            for cat in cat_contains_list:
+                restaurants = restaurants.filter(category__icontains=cat)
+                places = places.filter(category__icontains=cat)
+
+        if price_min:
+            restaurants = restaurants.filter(price_level__gte=price_min)
+            places = places.filter(price_level__gte=price_min)
+        if price_max:
+            restaurants = restaurants.filter(price_level__lte=price_max)
+            places = places.filter(price_level__lte=price_max)
+
+        if rating_min:
+            restaurants = restaurants.filter(rating__gte=rating_min)
+            places = places.filter(rating__gte=rating_min)
+        if rating_max:
+            restaurants = restaurants.filter(rating__lte=rating_max)
+            places = places.filter(rating__lte=rating_max)
+
+        if description_contains:
+            restaurants = restaurants.filter(description__icontains=description_contains)
+            places = places.filter(description__icontains=description_contains)
+
+        # Pass filtered results
+        context['all_restaurants'] = restaurants
+        context['all_places'] = places
+
+        return render(request, self.template_name, context)
